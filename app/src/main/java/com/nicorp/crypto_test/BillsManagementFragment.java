7bf060344ffcfc9c0e61a64d0d6280716c733a2b
NI Corp. <123ababab123ababab@gmail.com>
2024-06-28 18:57:18 +0300

    Update

--- app/src/main/java/com/nicorp/crypto_test/BillsManagementFragment.java
+++ app/src/main/java/com/nicorp/crypto_test/BillsManagementFragment.java
@@ -1,9 +1,11 @@
 package com.nicorp.crypto_test;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +20,7 @@ public class BillsManagementFragment extends Fragment {
 
     private RecyclerView recyclerView;
     private Bills_Management_Adapter adapter;
+    private ImageView backButton;
 
     @Nullable
     @Override
@@ -27,6 +30,8 @@ public class BillsManagementFragment extends Fragment {
         recyclerView = view.findViewById(R.id.rvBills);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        backButton = view.findViewById(R.id.backButton);
+
         List<Bill> bills = new ArrayList<>();
         bills.add(new Bill(R.drawable.qcoin, "First bill", "1000 QC", "~ 100$"));
         bills.add(new Bill(R.drawable.bitcoin, "Second bill", "0,001 BTC", "~ 63,79$"));
@@ -34,6 +39,24 @@ public class BillsManagementFragment extends Fragment {
 
         adapter = new Bills_Management_Adapter(bills, getContext());
         recyclerView.setAdapter(adapter);
+
+        // Set the appropriate drawable for the back button based on the theme
+        int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        if (currentNightMode == Configuration.UI_MODE_NIGHT_YES) {
+            backButton.setImageResource(R.drawable.arrow_left_w);
+        } else {
+            backButton.setImageResource(R.drawable.arrow_left_b);
+        }
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle back button click, for example, pop the back stack
+                if (getFragmentManager() != null) {
+                    getFragmentManager().popBackStack();
+                }
+            }
+        });
 
         return view;
     }
